32537c61678b583def0455c742dd390e2e81a251
Dody Hartanto <muharridody@gmail.com>
2023-07-13 13:43:59 +0800

    edit bg our values upper section

--- asset/js/dropdown.js
+++ asset/js/dropdown.js
@@ -4,12 +4,12 @@ function myDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.drop-button')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
+  // // Close the dropdown if the user clicks outside of it
+  // window.onclick = function(e) {
+  //   if (!e.target.matches('.drop-button')) {
+  //   var myDropdown = document.getElementById("myDropdown");
+  //     if (myDropdown.classList.contains('show')) {
+  //       myDropdown.classList.remove('show');
+  //     }
+  //   }
+  // }
